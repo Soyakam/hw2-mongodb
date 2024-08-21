9f523bb5575a64bc0886ab6f095851c2e41e2960
Soyakam <108721019+Soyakam@users.noreply.github.com>
2024-08-21 21:05:41 +0200

    123456

--- src/server.js
+++ src/server.js
@@ -2,17 +2,16 @@ import express from "express";
 import pino from "pino-http";
 import cors from "cors";
 import { env } from './utils/env.js';
-import router from './routers/index.js'; // Ensure this is correctly configured
+import router from './routers/index.js'; 
 import { errorHandler,  } from './middlewares/errorHandler.js';
 import { notFoundHandler } from"./middlewares/notFoundHandler.js";
 import cookieParser from 'cookie-parser';
 
-const PORT = Number(env('PORT', '4000'));
+const PORT = Number(env('PORT', '3000'));
 
 export const setupServer = () => {
   const app = express();
 
- 
   app.use(express.json());
   app.use(cors());
   app.use(cookieParser());
@@ -33,7 +32,7 @@ export const setupServer = () => {
     });
   });
   
-  app.use('/api', router); 
+  app.use(router); 
   
   app.use('*', notFoundHandler);
   
